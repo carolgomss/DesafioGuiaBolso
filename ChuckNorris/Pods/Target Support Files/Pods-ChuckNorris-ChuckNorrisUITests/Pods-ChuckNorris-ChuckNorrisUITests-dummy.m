3c0d488c1fc50d6c05b0067aee1906d04e2cd400
carolgomss <gomes_carolina.s@hotmail.com>
2021-02-10 12:46:59 -0300

    mudanca de gitignore para não inclusao de pods

--- ChuckNorris/Pods/Target Support Files/Pods-ChuckNorris-ChuckNorrisUITests/Pods-ChuckNorris-ChuckNorrisUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChuckNorris_ChuckNorrisUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChuckNorris_ChuckNorrisUITests
-@end
--- ChuckNorris/Pods/Target Support Files/Pods-ChuckNorrisTests/Pods-ChuckNorrisTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChuckNorrisTests : NSObject
-@end
-@implementation PodsDummy_Pods_ChuckNorrisTests
-@end
